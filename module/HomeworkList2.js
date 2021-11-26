13c6850233c87636c34fe85ccd57d780d8f34393
kanonkc <kanonkce@gmail.com>
2021-11-26 20:32:27 +0700

    Bubble Sort + String Formatting

--- module/HomeworkList2.js
+++ module/HomeworkList2.js
@@ -6,10 +6,10 @@ const DayName = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"]
 
 class Homework{
     constructor(id,date,label){
-        this.isValid = true
-        this.id = id
-        this.date = date
-        this.label = label
+        this.isValid = true     // Boolean
+        this.id = id            // String
+        this.date = date        // Integer [Array]
+        this.label = label      // String
         
         this.day_name = DayName[new Date(date[2],date[1]-1,date[0]).getDay()]
         this.timestamp = new Date(date[2],date[1]-1,date[0],23,59,59).getTime()
@@ -31,19 +31,53 @@ class HomeworkList{
         var raw_data = fs.readFileSync(`${DataPath}resource\\${file}`,'utf8').split('\n')
         for(var i=0;i<raw_data.length;i++){
             var spt_data = raw_data[i].split(' ')
-            // console.log(spt_data)
-            var instance = new Homework(spt_data[0],[Number(spt_data[1]),Number(spt_data[2]),2021],spt_data[4])
-            this.data = this.data.filter(i => i.id != instance.id)
-            if(instance.isValid){
-                this.data.push(instance)
-            }
-            if(i > 143){
-                console.log(instance)
+            var homework = new Homework(spt_data[0],[Number(spt_data[1]),Number(spt_data[2]),2021],spt_data[4])
+            this.data = this.data.filter(hw => hw.id != homework.id)
+            this.data.push(homework)
+        }
+        this.data = this.data.filter(hw => hw.day_left >= 0 && hw.isValid)
+        this.sort()
+    }
+
+    sort(){
+        // เดี๋ยวค่อยมาทำ Merge Sort ทีหลัง
+        for(var i=0;i<this.data.length;i++){
+            for(var j=0;j<this.data.length-1;j++){
+                if(this.data[j] > this.data[j+1]){
+                    var tmp = this.data[j]
+                    this.data[j] = this.data[j+1]
+                    this.data[j+1] = tmp
+                }
             }
         }
     }
+
+    list(){
+        var format_string = `:bookmark: **Homework List (${this.data.length}):**\n`
+        if(this.data.length == 0){return `${format_string}ไม่มีงาน!? เป็นไปได้ด้วยหรอครับเนี่ย!!??!`}
+
+        for(var i=0;i<this.data.length;i++){
+            var hw = this.data[i]
+            var vis_day = " วัน"
+
+            // Adding 0
+            if(Number(hw.date[0])<10){hw.date[0] = `0${hw.date[0]}`}
+            if(Number(hw.date[1])<10){hw.date[1] = `0${hw.date[1]}`}
+
+            if(hw.day_left==0){
+                hw.day_left = "เดี๋ยวนี้"
+                vis_day = ""
+            }
+            if(hw.day_left<10){hw.day_left = "0" + hw.day_left}
+            format_string += `[\`${hw.day_name}\`.\`${hw.date[0]}/${hw.date[1]}\`] ${hw.alert_icon} **(\`${hw.day_left}\`${vis_day})** 📝 \`[${hw.id}]\` \`${hw.label}\`\n`
+        }
+        return format_string
+    }
+
+
 }
 
 var hl = new HomeworkList('homeworklist.txt')
-// console.log(hl)
+console.log(hl.list())
+
 
