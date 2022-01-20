39a47d8be38ae4670ba3dbe6fb4f7d83a3ac8994
KanonKC <kanonkce@gmail.com>
2022-01-21 03:34:10 +0700

    add permission

--- commands/prefix.js
+++ commands/prefix.js
@@ -1,7 +1,7 @@
 module.exports = {
     name: "prefix",
     alias: [],
-    roleRequirement: [],
+    roleRequirement: [569178222495793153],
     execute: function(message,arg){
         message.channel.send(`My Prefix has been set to \`${arg[1]}\``)
         return ["PREFIX",arg[1]]
