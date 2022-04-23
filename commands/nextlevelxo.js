15ae7bedb676b8930dd958abd927cbf7bb5a81f8
KanonKC <kanonkce@gmail.com>
2022-04-24 01:05:20 +0700

    NXO Updated

--- commands/nextlevelxo.js
+++ commands/nextlevelxo.js
@@ -1,23 +1,31 @@
-const {NextLevelXO} = require('../module/NextLevelXO')
+const {NextLevelXO,getMatch,getNXOData,createMatch} = require('../module/NextLevelXO')
 const {getSpecial} = require('../module/DiscordSpecialTag')
 
-var NXO = new NextLevelXO('AAA','AAA','BBB','BBB')
+var NXO;
 
 module.exports = {
     name: "nextlevelxo",
     alias: ['nextlevelxo','nxo'],
     roleRequirement: [],
-    execute: function(message,arg){
+    execute: async function(message,arg){
         switch(arg[1]){
             case 'create':
                 var p2_uid = arg[2].slice(2,-1)
                 var p2_username = message.guild.members.cache.get(p2_uid).user.username
-                NXO = new NextLevelXO(message.author.id,message.author.username,p2_uid,p2_username)
+                var match = await createMatch(message.author.id,message.author.username,p2_uid,p2_username)
+                if(match.result == 0){
+                    NXO = new NextLevelXO(match.data)
+                }
                 message.channel.send(NXO.showBoard())
                 message.channel.send(`Player #${NXO.turn+1} <@${NXO.player[NXO.turn].uid}> Start`)
                 break
-            
             case 'play':
+                var match_result = await getMatch(message.author.id)
+                if(match_result.status != 0){
+                    message.channel.send(`User not found!`)
+                    break
+                }
+                NXO = new NextLevelXO(match_result.data)
                 if(Number(arg[2]) <= 0 || Number(arg[2]) > 3 || Number(arg[3]) <= 0 || Number(arg[3]) > 3 || Number(arg[4]) <= 0 || Number(arg[4]) > 5){
                     message.channel.send(`Error Input`)
                 }
@@ -27,6 +35,7 @@ module.exports = {
                 }
                 else{
                     message.channel.send(NXO.showBoard())
+                    NXO.endgame()
                     if(result == 1 || result == 2){
                         message.channel.send(`Player #${result} WIN!`)
                     }
@@ -37,6 +46,7 @@ module.exports = {
                 if(result < 1){
                     message.channel.send(`Player #${NXO.turn+1} <@${NXO.player[NXO.turn].uid}> Turn`)
                 }
+                NXO.updateMatchStatus()
                 break
         }
         return 0
--- module/NextLevelXO.js
+++ module/NextLevelXO.js
@@ -1,37 +1,54 @@
+const axios = require('axios')
 const TokenIcon = ['❌','⭕']
+const IP_ADDRESS = "http://localhost:8000/nextlevelxo"
+
+async function getMatch(uid){
+    const response = await axios.get(`${IP_ADDRESS}/uid?uid=${uid}`)
+    return response.data
+}
+
+async function getNXOData(){
+    const response = await axios.get(IP_ADDRESS)
+    return response.data
+}
+
+async function createMatch(uid1,player1,uid2,player2){
+    const response = await axios.post(IP_ADDRESS,{
+        uid1: uid1,
+        username1: player1,
+        uid2: uid2,
+        username2: player2
+    })
+    return response.data
+}
 
 class Player{
-    constructor(uid,name){
+    constructor(uid,name,token){
         this.name = name
         this.uid = uid
-        this.token = [1,1,1,1,1]
+        this.token = token
     }
 }
 
 class NextLevelXO{
-    constructor(uid1,name1,uid2,name2){
-        this.player = [new Player(uid1,name1),new Player(uid2,name2)]
-        this.board = {
-            row: 3,
-            col: 3,
-            owner: [
-                ['➖','➖','➖'],
-                ['➖','➖','➖'],
-                ['➖','➖','➖']
-            ],
-            level: [
-                ['-','-','-'],
-                ['-','-','-'],
-                ['-','-','-']
-            ]
-        }
-        this.turn = Math.floor(Math.random()*2)
+    constructor(match){
+        this.match_id = match.match_id
+        this.isValid = match.isValid
+        this.player = match.player/* [
+            new Player(match.player[0].uid,match.player[0].username,match.player[0].token),
+            new Player(match.player[1].uid,match.player[1].username,match.player[1].token)
+        ] */
+        this.board = match.board /* {
+            owner: match.board.owner,
+            level: match.board.level
+        } */
+        this.turn = match.turn /* match.turn */
     }
 
     showBoard(){
         var format_string = "```\n"
-        for(var i=0;i<this.board.row;i++){
-            for(var j=0;j<this.board.col;j++){
+        for(var i=0;i<3;i++){
+            for(var j=0;j<3;j++){
                 var increase_level = this.board.level[i][j]
                 if(increase_level != '-')
                     increase_level++
@@ -39,7 +56,7 @@ class NextLevelXO{
             }
             format_string += "|\n"
         }
-        format_string += `\n❌ Player #1: ${this.player[0].name}\n`
+        format_string += `\n❌ Player #1: ${this.player[0].username}\n`
         format_string += "Token Available: 1 > "
         for(var i=0;i<5;i++){
             if(this.player[0].token[i])
@@ -49,7 +66,7 @@ class NextLevelXO{
         }
         format_string += "< 5"
         format_string += '\n\n'
-        format_string += `⭕ Player #2: ${this.player[1].name}\n`
+        format_string += `⭕ Player #2: ${this.player[1].username}\n`
         format_string += "Token Available: 1 > "
         for(var i=0;i<5;i++){
             if(this.player[1].token[i])
@@ -97,6 +114,7 @@ class NextLevelXO{
     }
 
     isWin(){
+        this.isValid = false
         for(var i=0;i<3;i++){
             if(this.board.owner[i][0] != '➖' && this.board.owner[i][0] == this.board.owner[i][1] && this.board.owner[i][1] == this.board.owner[i][2]) return 1
             if(this.board.owner[0][i] != '➖' && this.board.owner[0][i] == this.board.owner[1][i] && this.board.owner[1][i] == this.board.owner[2][i]) return 1
@@ -108,19 +126,54 @@ class NextLevelXO{
 
         for(var i=0;i<3;i++){
             for(var j=0;j<3;j++){
-                if(this.board.owner[i][j] != '➖')
+                if(this.board.owner[i][j] != '➖'){
+                    this.isValid = true
                     return 0
+                }
             }
         }
         return 2
     }
+
+    async updateMatchStatus(){
+        const response = await axios.patch(IP_ADDRESS,{
+            match_id:   this.match_id,
+            isValid:    this.isValid,
+            player:     this.player,
+            board:      this.board,
+            turn:       this.turn
+        })
+        return response
+    }
+
+    async endgame(){
+        const response = await axios.patch(`${IP_ADDRESS}/disable?uid=${this.player[this.turn].uid}`)
+        return response
+    }
 }
 
+// (async()=>{
+//     let result = await getMatch('12345')
+//     result = result.data.data
+//     var game = new NextLevelXO(result.match_id,result.isValid,result.player,result.board,result.turn)
+//     var res = game.placeBlock(1,1,3)
+//     console.log(res)
+//     console.log(game.showBoard())
+//     var resp = game.updateMatchStatus()
+//     console.log("DONE",resp)
+// })()
+
+
 module.exports = {
+    getMatch,
+    getNXOData,
+    createMatch,
     NextLevelXO
 }
 
-// game = new NextLevelXO('1','AAA','2','BBBB')
+
+
+
 // game.placeBlock(1,2,3)
 // game.placeBlock(1,1,4)
 // game.placeBlock(0,0,2)
