2fe778f5cd55d6d4d2732b33c63c0df15a491820
KanonKC <kanonkce@gmail.com>
2022-06-26 13:55:14 +0700

    Update MessageDetector

--- module/MessageDetector.js
+++ module/MessageDetector.js
@@ -5,8 +5,10 @@ const {getSpecial} = require('./DiscordSpecialTag')
 
 module.exports = {
     execute: async function(message){
-        var special = getSpecial(message.content)
-        var sample = {
+        let special = getSpecial(message.content)
+        let attachments_res = message.attachments.map(attach => attach.attachment)
+
+        let sample = {
             username: message.author.username,
             uid: message.author.id,
             channel: message.channelId,
@@ -15,7 +17,8 @@ module.exports = {
             mentions_user: special.mentions_user,
             mentions_channel: special.mentions_channel,
             mentions_role: special.mentions_role,
-            message_id: message.id
+            message_id: message.id,
+            attachments: attachments_res
         }
         postMessage(sample)
     }
