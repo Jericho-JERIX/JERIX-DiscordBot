44f8d3bd08b31494582cde879915fc89937847bb
KanonKC <kanonkce@gmail.com>
2022-07-17 20:04:31 +0700

    update defer

--- interaction/homeworklist.js
+++ interaction/homeworklist.js
@@ -3,8 +3,8 @@ module.exports = {
     alias: ['homeworklist'],
     roleRequirement: [],
     execute: async function(interact,arg,Command){
-        interact.deferUpdate()
         var result = await Command.homework.getList(arg[1].toUpperCase(),interact.channelId)
         interact.message.edit(result)
+        interact.deferUpdate()
     }
 }
