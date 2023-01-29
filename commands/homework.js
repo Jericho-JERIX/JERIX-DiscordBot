3fd46645a711fe384c3a4cbc373056cf6257898c
KanonKC <kanonkce@gmail.com>
2023-01-29 21:06:41 +0700

    Fix: text

--- commands/homework.js
+++ commands/homework.js
@@ -84,7 +84,7 @@ const Homeworklist = {
     Card: (instance) => {
         const hw = new Homework(instance)
         if(hw.day_left == 0){
-            return `[\`${hw.day_name}\`.\`${fixSpace(hw.date,2,'0')}/${fixSpace(hw.month,2,'0')}\`] ${hw.alert_icon} **(เดี๋ยวนี้!)** ${hw.type_icon} \`[${fixSpace(hw.id,4,'0')}]\` \`${hw.label}\``
+            return `[\`${hw.day_name}\`.\`${fixSpace(hw.date,2,'0')}/${fixSpace(hw.month,2,'0')}\`] ${hw.alert_icon} **(\`เดี๋ยวนี้!\`)** ${hw.type_icon} \`[${fixSpace(hw.id,4,'0')}]\` \`${hw.label}\``
         }
         return `[\`${hw.day_name}\`.\`${fixSpace(hw.date,2,'0')}/${fixSpace(hw.month,2,'0')}\`] ${hw.alert_icon} **(\`${fixSpace(hw.day_left,3)}\` วัน)** ${hw.type_icon} \`[${fixSpace(hw.id,4,'0')}]\` \`${hw.label}\``
     },
